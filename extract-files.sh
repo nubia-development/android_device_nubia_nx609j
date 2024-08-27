99181321a544378823023ad50e0abec7774fd81c
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:05:28 +0300

    nx609j: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,44 +1,60 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
-    # Patch blobs for VNDK
-    vendor/lib/hw/camera.msm8998.so)
-        "${PATCHELF}" --remove-needed "libgui.so" "${2}"
-        "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
-        "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
-        ;;
-    vendor/lib/libnubia_effect.so | vendor/lib64/libnubia_effect.so)
-        "${PATCHELF}" --remove-needed "libgui.so" "${2}"
-        ;;
-    vendor/lib/libNubiaImageAlgorithm.so)
-        "${PATCHELF}" --remove-needed  "libjnigraphics.so" "${2}"
-        "${PATCHELF}" --remove-needed  "libnativehelper.so" "${2}"
-        "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
-        "${PATCHELF}" --add-needed "libNubiaImageAlgorithm_shim.so" "${2}"
-        ;;
-    vendor/lib/libarcsoft_picauto.so)
-        "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
-        ;;
-    vendor/lib64/libgf_hal.so)
-        "${PATCHELF}" --remove-needed "libpowermanager.so" "${2}"
-        ;;
-    vendor/lib64/libgoodixfingerprintd_binder.so)
-        "${PATCHELF}" --add-needed "libbinder_shim.so" "${2}"
-        ;;
-    vendor/lib64/android.hardware.gatekeeper@1.0.so | vendor/lib64/vendor.goodix.hardware.fingerprint@1.0.so)
-        "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
-        ;;
-    vendor/lib/libSonyIMX318PdafLibrary.so | vendor/lib/libarcsoft_beautyshot.so | vendor/lib/libarcsoft_beautyshot_image_algorithm.so | vendor/lib/libarcsoft_beautyshot_video_algorithm.so | vendor/lib/libarcsoft_high_dynamic_range.so | vendor/lib/libarcsoft_low_light_shot.so | vendor/lib/libarcsoft_night_shot.so | vendor/lib/libarcsoft_picauto.so | vendor/lib64/libarcsoft_beautyshot.so | vendor/lib64/libarcsoft_beautyshot_image_algorithm.so | vendor/lib64/libarcsoft_beautyshot_video_algorithm.so | vendor/lib64/libarcsoft_low_light_shot.so | vendor/lib64/libarcsoft_night_shot.so)
-        "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
-        ;;
+        # Patch blobs for VNDK
+        vendor/lib/hw/camera.msm8998.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --remove-needed "libgui.so" "${2}"
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
+            ;;
+        vendor/lib/libnubia_effect.so | vendor/lib64/libnubia_effect.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --remove-needed "libgui.so" "${2}"
+            ;;
+        vendor/lib/libNubiaImageAlgorithm.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --remove-needed  "libjnigraphics.so" "${2}"
+            "${PATCHELF}" --remove-needed  "libnativehelper.so" "${2}"
+            "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
+            "${PATCHELF}" --add-needed "libNubiaImageAlgorithm_shim.so" "${2}"
+            ;;
+        vendor/lib/libarcsoft_picauto.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
+            ;;
+        vendor/lib64/libgf_hal.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --remove-needed "libpowermanager.so" "${2}"
+            ;;
+        vendor/lib64/libgoodixfingerprintd_binder.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --add-needed "libbinder_shim.so" "${2}"
+            ;;
+        vendor/lib64/android.hardware.gatekeeper@1.0.so | vendor/lib64/vendor.goodix.hardware.fingerprint@1.0.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+            ;;
+        vendor/lib/libSonyIMX318PdafLibrary.so | vendor/lib/libarcsoft_beautyshot.so | vendor/lib/libarcsoft_beautyshot_image_algorithm.so | vendor/lib/libarcsoft_beautyshot_video_algorithm.so | vendor/lib/libarcsoft_high_dynamic_range.so | vendor/lib/libarcsoft_low_light_shot.so | vendor/lib/libarcsoft_night_shot.so | vendor/lib/libarcsoft_picauto.so | vendor/lib64/libarcsoft_beautyshot.so | vendor/lib64/libarcsoft_beautyshot_image_algorithm.so | vendor/lib64/libarcsoft_beautyshot_video_algorithm.so | vendor/lib64/libarcsoft_low_light_shot.so | vendor/lib64/libarcsoft_night_shot.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+            ;;
+        *)
+            return 1
+            ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
@@ -53,7 +69,8 @@ set -e
 export DEVICE=nx609j
 export DEVICE_COMMON=msm8998-common
 export VENDOR=nubia
+export VENDOR_COMMON=${VENDOR}
 
 export DEVICE_BRINGUP_YEAR=2017
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
 
 set -e
 
@@ -12,7 +17,8 @@ set -e
 export DEVICE=nx609j
 export DEVICE_COMMON=msm8998-common
 export VENDOR=nubia
+export VENDOR_COMMON=${VENDOR}
 
 export DEVICE_BRINGUP_YEAR=2017
 
-"./../../${VENDOR}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/setup-makefiles.sh" "$@"
